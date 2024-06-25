fa1694084036a28190c3170454f4b21fa1636d52
Daniel Aston <daniel.aston@live.co.uk>
2024-06-25 16:51:36 +0100

    Add users and reviews controllers

---
+++ Controllers/reviewController.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+function getBooksReviews(req, res) {}
+
+function postReview(req, res) {}
+
+export default { getBooksReviews, postReview };
---
+++ Controllers/userController.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+function getUser(req, res){}
+
+function getBorrowedBooks(req, res){}
+
+function getLogged(req, res){}
+
+function postSignUp(req, res){}
+
+function postLogin(req, res){}
+
+export default {
+  getUser,
+  getBorrowedBooks,
+  getLogged,
+  postSignUp,
+  postLogin
+}
