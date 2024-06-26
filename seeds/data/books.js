e4be42569ecba41eba65944d8b9667caf46a55e7
Daniel Aston <daniel.aston@live.co.uk>
2024-06-26 11:17:47 +0100

    Update seeds

--- seeds/data/books.js
+++ seeds/data/books.js
@@ -4,63 +4,63 @@ export default [
     blurb:
       "A young girl named Alice falls through a rabbit hole into a fantasy world populated by peculiar, anthropomorphic creatures.",
     author: "Lewis Carroll",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Alice-in-wonderland/Alice-in-wonderland.txt",
+    CoverImagePath: "../../assets/Books/Alice-in-wonderland/images/cover.jpg",
   },
   {
     title: "A Room with a View",
     blurb:
       "A young Englishwoman named Lucy Honeychurch embarks on a journey of self-discovery during a trip to Italy.",
     author: "E. M. Forster",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/A-room-with-a-view/A-room-with-a-view.txt",
+    CoverImagePath: "../../assets/Books/A-room-with-a-view/images/cover.jpg",
   },
   {
     title: "Dracula",
     blurb:
       "The story of Count Dracula's attempt to move from Transylvania to England so he may find new blood and spread the undead curse.",
     author: "Bram Stoker",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Dracula/Dracula.txt",
+    CoverImagePath: "../../assets/Books/Dracula/images/cover.jpg",
   },
   {
     title: "Frankenstein",
     blurb:
       "Victor Frankenstein, a young scientist, creates a grotesque creature in an unorthodox scientific experiment.",
     author: "Mary Shelley",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Frankenstien/Frankenstein.txt",
+    CoverImagePath: "../../assets/Books/Frankenstien/84-cover.png",
   },
   {
     title: "Little Women",
     blurb:
       "The lives, loves, and tribulations of four sisters growing up during the American Civil War.",
     author: "Louisa May Alcott",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Little-Women/Little-women.txt",
+    CoverImagePath: "../../assets/Books/Little-Women/images/cover.png",
   },
   {
     title: "Middlemarch",
     blurb:
       "A comprehensive and sympathetic examination of the lives of the residents of a fictional English town.",
     author: "George Eliot",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Middlemarch/Middlemarch.txt",
+    CoverImagePath: "../../assets/Books/Middlemarch/images/cover.jpg",
   },
   {
     title: "Moby Dick",
     blurb:
       "The narrative of Captain Ahab's obsessive quest to seek revenge on Moby Dick, the white whale that bit off his leg.",
     author: "Herman Melville",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Moby-Dick/Moby-Dick.txt",
+    CoverImagePath: "../../assets/Books/Moby-Dick/2701-cover.png",
   },
   {
     title: "Romeo and Juliet",
     blurb:
       "The tragic love story of Romeo Montague and Juliet Capulet, young lovers whose deaths ultimately reconcile their feuding families.",
     author: "William Shakespeare",
-    textFilePath: "",
-    CoverImagePath: "",
+    textFilePath: "../../assets/Books/Romeo-and-Juliet/Romeo-and-Juliet.txt",
+    CoverImagePath: "../../assets/Books/Romeo-and-Juliet/images/cover.jpg",
   },
 ];
