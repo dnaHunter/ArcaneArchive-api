ecd0e2ba9a2e454520e1312d2edd0a7c2f826b05
Daniel Aston <daniel.aston@live.co.uk>
2024-06-26 17:06:49 +0100

    update seeds

--- seeds/data/books.js
+++ seeds/data/books.js
@@ -5,7 +5,7 @@ export default [
       "A young girl named Alice falls through a rabbit hole into a fantasy world populated by peculiar, anthropomorphic creatures.",
     author: "Lewis Carroll",
     textFilePath: "../../assets/Books/Alice-in-wonderland/Alice-in-wonderland.txt",
-    CoverImagePath: "public/Covers/alice-in-wonderland-cover.jpg",
+    CoverImagePath: "Covers/alice-in-wonderland-cover.jpg",
   },
   {
     title: "A Room with a View",
@@ -13,7 +13,7 @@ export default [
       "A young Englishwoman named Lucy Honeychurch embarks on a journey of self-discovery during a trip to Italy.",
     author: "E. M. Forster",
     textFilePath: "../../assets/Books/A-room-with-a-view/A-room-with-a-view.txt",
-    CoverImagePath: "public/Covers/a-room-with-a-view-cover.jpg",
+    CoverImagePath: "Covers/a-room-with-a-view-cover.jpg",
   },
   {
     title: "Dracula",
@@ -21,7 +21,7 @@ export default [
       "The story of Count Dracula's attempt to move from Transylvania to England so he may find new blood and spread the undead curse.",
     author: "Bram Stoker",
     textFilePath: "../../assets/Books/Dracula/Dracula.txt",
-    CoverImagePath: "public/Covers/dracula-cover.jpg",
+    CoverImagePath: "Covers/dracula-cover.jpg",
   },
   {
     title: "Frankenstein",
@@ -29,7 +29,7 @@ export default [
       "Victor Frankenstein, a young scientist, creates a grotesque creature in an unorthodox scientific experiment.",
     author: "Mary Shelley",
     textFilePath: "../../assets/Books/Frankenstien/Frankenstein.txt",
-    CoverImagePath: "public/Covers/Frankenstein-cover.png",
+    CoverImagePath: "Covers/Frankenstein-cover.png",
   },
   {
     title: "Little Women",
@@ -37,7 +37,7 @@ export default [
       "The lives, loves, and tribulations of four sisters growing up during the American Civil War.",
     author: "Louisa May Alcott",
     textFilePath: "../../assets/Books/Little-Women/Little-women.txt",
-    CoverImagePath: "public/Covers/little-woman-cover.png",
+    CoverImagePath: "Covers/little-woman-cover.png",
   },
   {
     title: "Middlemarch",
@@ -45,7 +45,7 @@ export default [
       "A comprehensive and sympathetic examination of the lives of the residents of a fictional English town.",
     author: "George Eliot",
     textFilePath: "../../assets/Books/Middlemarch/Middlemarch.txt",
-    CoverImagePath: "public/Covers/middlemarch-cover.jpg",
+    CoverImagePath: "Covers/middlemarch-cover.jpg",
   },
   {
     title: "Moby Dick",
@@ -53,7 +53,7 @@ export default [
       "The narrative of Captain Ahab's obsessive quest to seek revenge on Moby Dick, the white whale that bit off his leg.",
     author: "Herman Melville",
     textFilePath: "../../assets/Books/Moby-Dick/Moby-Dick.txt",
-    CoverImagePath: "public/Covers/moby-dick-cover.png",
+    CoverImagePath: "Covers/moby-dick-cover.png",
   },
   {
     title: "Romeo and Juliet",
@@ -61,6 +61,6 @@ export default [
       "The tragic love story of Romeo Montague and Juliet Capulet, young lovers whose deaths ultimately reconcile their feuding families.",
     author: "William Shakespeare",
     textFilePath: "../../assets/Books/Romeo-and-Juliet/Romeo-and-Juliet.txt",
-    CoverImagePath: "public/Covers/romeo-and-juliet-cover.jpg",
+    CoverImagePath: "Covers/romeo-and-juliet-cover.jpg",
   },
 ];
