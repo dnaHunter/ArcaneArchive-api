564b8c5e871dfe7d2e3632d91b19fbce9fc061c2
Daniel Aston <daniel.aston@live.co.uk>
2024-07-03 17:04:58 +0100

    Add unlocker and run it every 2 mins

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bookRoutes from "./Routes/bookRoutes.js";
 import userRoutes from "./Routes/userRoutes.js";
 import reviewRoutes from "./Routes/reviewRoutes.js";
 import "dotenv/config";
+import { unlocker } from "./utils/utils.js";
 
 const PORT = process.env.PORT;
 const BACKEND_URL = process.env.BACKEND_URL;
@@ -16,10 +17,13 @@ app.use(express.json());
 
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 
+setInterval(unlocker, 120000);
+
 app.use("/books", bookRoutes);
 app.use("/users", userRoutes);
 app.use("/reviews", reviewRoutes);
 
 app.listen(PORT, () => {
+  unlocker()
   console.log(`Listening on ${BACKEND_URL}:${PORT}`);
 });
---
+++ utils/utils.js
@@ -0,0 +1,13 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+const knex = initKnex(configuration);
+
+export async function unlocker() {
+  try {
+    const lockedInfo = await knex("books")
+      .update({ locked: false, lockedUntil: null })
+      .where(knex.raw("lockedUntil < NOW()"));
+  } catch (error) {
+    console.error(error);
+  }
+}
