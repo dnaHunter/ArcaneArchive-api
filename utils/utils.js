1c9d827339f6e1c474ffa1acc94db2a5a568b7c6
Daniel Aston <daniel.aston@live.co.uk>
2024-07-08 13:36:03 +0100

    Add lockedby id to unlocker

--- utils/utils.js
+++ utils/utils.js
@@ -5,7 +5,7 @@ const knex = initKnex(configuration);
 export async function unlocker() {
   try {
     const lockedInfo = await knex("books")
-      .update({ locked: false, lockedUntil: null })
+      .update({ locked: false, lockedUntil: null, lockedBy_id: null })
       .where(knex.raw("lockedUntil < NOW()"));
   } catch (error) {
     console.error(error);
